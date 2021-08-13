af8519f82b944800538f5e5b656444b9f486aa3b
etaoxing <etaoxing@users.noreply.github.com>
2021-08-13 09:05:04 -0400

    add except around gym.register

--- kitchen_env/__init__.py
+++ kitchen_env/__init__.py
@@ -1,20 +1,26 @@
 from gym.envs.registration import register
 
-from .kitchen_v0 import Kitchen_v0
+try:
+    from .kitchen_v0 import Kitchen_v0
 
-register(
-    id='kitchen-v0',
-    entry_point='kitchen_env.kitchen_v0:Kitchen_v0',
-    max_episode_steps=280,
-)
+    register(
+        id='kitchen-v0',
+        entry_point='kitchen_env.kitchen_v0:Kitchen_v0',
+        max_episode_steps=280,
+    )
+except:
+    pass
 
-from .kitchen_v1 import Kitchen_v1
+try:
+    from .kitchen_v1 import Kitchen_v1
 
-register(
-    id='kitchen-v1',
-    entry_point='kitchen_env.kitchen_v1:Kitchen_v1',
-    max_episode_steps=280,
-)
+    register(
+        id='kitchen-v1',
+        entry_point='kitchen_env.kitchen_v1:Kitchen_v1',
+        max_episode_steps=280,
+    )
+except:
+    pass
 
 
 from .mujoco.worldgen import MujocoWorldgenKitchenEnvWrapper
