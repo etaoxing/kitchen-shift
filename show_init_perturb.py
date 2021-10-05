b035f20c088527402d09476cbcbae8d2a476b742
etaoxing <etaoxing@users.noreply.github.com>
2021-10-05 13:07:21 -0400

    show object perturb

--- show_init_perturb.py
+++ show_init_perturb.py
@@ -30,6 +30,10 @@ init_perturb_robot_ratio = 0.04
 init_qpos1 = env.init_qpos[:].copy()
 init_qpos2 = env.init_qpos[:].copy()
 
+init_perturb_object_ratio = 0.04
+init_qpos3 = env.init_qpos[:].copy()
+init_qpos4 = env.init_qpos[:].copy()
+
 for i in range(env.N_DOF_ROBOT):
     init_qpos1[i] += init_perturb_robot_ratio * env.robot.robot_pos_bound[: env.N_DOF_ROBOT, 0][i]
     env.set_init_qpos(init_qpos1)
@@ -47,3 +51,24 @@ for i in range(env.N_DOF_ROBOT):
     state = env.reset()
     img = env.render(mode='rgb_array', height=rs[0], width=rs[1])
     cv2.imwrite(f'assets/init_perturb/init_perturb_robot_ratio-high-{i}.png', img[:, :, ::-1])
+
+
+for i in range(env.N_DOF_OBJECT):
+    init_qpos3[i + env.N_DOF_ROBOT] += init_perturb_object_ratio
+    env.set_init_qpos(init_qpos3)
+    env.set_init_noise_params(None, 0, 0, 'generator')
+
+    state = env.reset()
+    action = env.step(env.action_space.sample())
+    img = env.render(mode='rgb_array', height=rs[0], width=rs[1])
+    cv2.imwrite(f'assets/init_perturb/init_perturb_object_ratio-low-{i}.png', img[:, :, ::-1])
+
+for i in range(env.N_DOF_OBJECT):
+    init_qpos4[i + env.N_DOF_ROBOT] -= init_perturb_object_ratio
+    env.set_init_qpos(init_qpos4)
+    env.set_init_noise_params(None, 0, 0, 'generator')
+
+    state = env.reset()
+    action = env.step(env.action_space.sample())
+    img = env.render(mode='rgb_array', height=rs[0], width=rs[1])
+    cv2.imwrite(f'assets/init_perturb/init_perturb_object_ratio-high-{i}.png', img[:, :, ::-1])
